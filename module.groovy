7fca9da3df54e6163c0d5a43240d37dc7ddf220c
Cervator <cervator@gmail.com>
2017-10-29 22:19:38 -0400

    Ooooone more thing. Or two more things. Prep the prepareStuff task better and bring in the latest module utility

--- module.groovy
+++ module.groovy
@@ -1,81 +1,3 @@
-// TODO: Credit original authors (MaxBorsch PR #2634 and smsunarto PR #2657
-// TODO: Also separately credit #2727 by oniatus as inspiration to get going with Gradle/Groovy
-
-/*
-// Interactive Dynamic task definition for creating a new module (from scratch, rather than cloning from GitHub)
-// Sample command: gradlew createModuleMyStuff
-tasks.addRule("Pattern: createModule<ID>,<VERSION>,<AUTHOR>,<DESCRIPTION>") { String taskName ->
-    if (taskName.startsWith("createModule")) {
-
-        // Split arguments by comma
-        def args = (taskName - 'createModule').tokenize (',')
-
-        // Use provided argument or request from the user
-        def createModule_repo = args[0] ?: getUserString ('Enter module name:')
-        def createModule_version = args[1] ?: getUserString ('Enter initial module version:')
-        def createModule_author = args[2] ?: getUserString ('Enter author name:')
-        def createModule_description = args[3] ?: getUserString ('Enter module description:')
-
-        // Here's the actual task definition for each dynamically created task of name taskName
-        task (taskName, type: GitInit) {
-            description = 'Creates template source for a given module to the local project and preps a local Git repo'
-
-            if (!createModule_repo){
-                throw new GradleException ("Aborted createModule! No module name.")
-            }
-
-            // Repo name is the dynamic part of the task name
-            def repo = createModule_repo
-            def parentDir = 'modules'
-
-            // Default GitHub account to use. Supply with -PgithubAccount="TargetAccountName" or via gradle.properties
-            def githubHome = 'Terasology'
-
-            // Command line parameter has precedence - if it is set we do not check gradle.properties
-            if (project.hasProperty('githubAccount')) {
-                githubHome = githubAccount
-            } else if (project.hasProperty('alternativeGithubHome')) {
-                githubHome =alternativeGithubHome
-            }
-
-            def destination = file("$parentDir/$repo")
-
-            // Don't create this repo if we already have a directory by that name (also determines Gradle UP-TO-DATE)
-            enabled = !destination.exists()
-            println "createModule requested for $repo linked to $githubHome on GitHub - exists already? " + !enabled
-
-            // Do the actual creation if we don't have the directory already
-            if (enabled) {
-
-                println "Creating $repo locally"
-                destination.mkdir()
-                destinationPath = destination
-
-                // TODO: Add in the local mapping to a remote ref definition. Needs support in the Gradle-Git plugin
-                //uri = "https://github.com/$githubAccount/" + repo + ".git"
-                bare = false
-
-                // Copy in some template stuff for the new module
-                doLast {
-                    new File(destination, 'build.gradle') << new File(rootDir, 'modules/Core/build.gradle').text
-                    // TODO : Add in the logback.groovy from engine\src\test\resources\logback.groovy ? Local dev only, Jenkins will use the one inside engine-tests.jar. Also add to .gitignore
-                    def moduleManifest = new File (destination, 'module.txt')
-                    def moduleText = new File(templatesDir, 'module.txt').text
-                    moduleText = moduleText
-                            .replaceAll('MODULENAME', createModule_repo)
-                            .replaceAll('VERSION', createModule_version)
-                            .replaceAll('AUTHOR', createModule_author)
-                            .replaceAll('DESCRIPTION', createModule_description)
-
-                    moduleManifest << moduleText
-                    new File(destination, '.gitignore') << new File(templatesDir, '.gitignore').text
-                }
-            }
-        }
-    }
-}
-*/
-
 // We use GrGit for interacting with Git. This gets a hold of it as a dependency like Gradle would
 // TODO: Consider if we should do something to fix/suppress the SLF4J warning that gets logged on first usage?
 @GrabResolver(name = 'jcenter', root = 'http://jcenter.bintray.com/')
@@ -123,7 +45,7 @@ def retrieve(String[] modules, boolean recurse) {
  * @param recurse whether to also retrieve its dependencies (if so then recurse back into retrieve)
  */
 def retrieveModule(String module, boolean recurse) {
-    File targetDir = new File("modulesTEST/$module") //TODO: Adjust after testing - moduleTEST
+    File targetDir = new File("modules/$module")
     println "Request to retrieve module $module would store it at $targetDir - exists? " + targetDir.exists()
 
     if (targetDir.exists()) {
@@ -140,7 +62,7 @@ def retrieveModule(String module, boolean recurse) {
         // TODO: Temporary until build.gradle gets removed from module directories (pending Cervator work)
         File targetBuildGradle = new File(targetDir, 'build.gradle')
         targetBuildGradle.delete()
-        targetBuildGradle << new File('modulesTEST/Core/build.gradle').text //TODO: Adjust after testing - moduleTEST
+        targetBuildGradle << new File('templates/build.gradle').text
 
         File moduleManifest = new File(targetDir, 'module.txt')
         if (!moduleManifest.exists()) {
@@ -192,6 +114,67 @@ String[] readModuleDependencies(File targetModuleInfo) {
 }
 
 /**
+ * Creates a new module with the given name and adds the necessary .gitignore,
+ * build.gradle and module.txt files.
+ * @param name the name of the module to be created
+ */
+def createModule(String name) {
+    // Check if the module already exists. If not, create the module directory
+    File targetDir = new File("modules/$name")
+    if (targetDir.exists()) {
+        println "Target directory already exists. Aborting."
+        return
+    }
+    println "Creating target directory"
+    targetDir.mkdir()
+
+    // Add gitignore
+    println "Creating .gitignore"
+    File gitignore = new File(targetDir, ".gitignore")
+    def gitignoreText = new File("templates/.gitignore").text
+    gitignore << gitignoreText
+
+    // Add build.gradle (temporary until it gets removed)
+    println "Creating build.gradle"
+    File buildGradle = new File(targetDir, "build.gradle")
+    def buildGradleText = new File("templates/build.gradle").text
+    buildGradle << buildGradleText
+
+    // Add module.txt
+    println "Creating module.txt"
+    File moduleManifest = new File(targetDir, "module.txt")
+    def moduleText = new File("templates/module.txt").text
+    moduleManifest << moduleText.replaceAll('MODULENAME', name)
+
+    // Initialize git
+    Grgit.init dir: targetDir, bare: false
+}
+
+/**
+ * Update a given module.
+ * @param name the name of the module to update
+ */
+def updateModule(String name) {
+    println "Attempting to update module $name"
+    File targetDir = new File("modules/$name")
+    if (!targetDir.exists()) {
+        println "Module \"$name\" not found"
+        return
+    }
+
+    def moduleGit = Grgit.open(dir: targetDir)
+    def clean = moduleGit.status().clean
+    println "Is \"$name\" clean? $clean"
+    if (!clean) {
+        println "Module has uncommitted changes. Aborting."
+        return
+    }
+
+    println "Updating module $name"
+    moduleGit.pull remote: "origin"
+}
+
+/**
  * Accepts input from the user, showing a descriptive prompt.
  * @param prompt the prompt to show the user
  */
@@ -211,6 +194,7 @@ def printUsage() {
     println "Utility script for interacting with modules. Available sub commands:"
     println "- 'get' - retrieves one or more modules in source form (separate with spaces)"
     println "- 'recurse' - retrieves the given module(s) *and* their dependencies in source form"
+    println "- 'create' - creates a new module"
     println ""
     println "Example: 'groovyw module recurse GooeysQuests Sample' - would retrieve those modules plus their dependencies"
     println "*NOTE*: Module names are case sensitive"
@@ -236,8 +220,8 @@ if (args.length == 0) {
         case "recurse":
             recurse = true
             println "We're retrieving recursively (all the things depended on too)"
-            // We just fall through here to the get logic after setting a boolean
-        //noinspection GroovyFallthrough
+    // We just fall through here to the get logic after setting a boolean
+    //noinspection GroovyFallthrough
         case "get":
             println "Preparing to get one or more modules"
             if (args.length == 1) {
@@ -258,8 +242,49 @@ if (args.length == 0) {
             break
         case "create":
             println "We're doing a create"
-            String someParam = getUserString("Please enter a thing")
-            println "User return was: $someParam"
+            String name = ""
+
+            // Get new module's name
+            if (args.length > 2) {
+                println "Received more than one argument. Aborting."
+                break
+            } else if (args.length == 2) {
+                name = args[1]
+            } else {
+                name = getUserString("Enter module name: ")
+            }
+            println "User wants to create a module named: $name"
+
+            createModule(name)
+
+            println "Created module named $name"
+            break
+        case "update":
+            println "We're updating modules"
+            String[] moduleList = []
+            if (args.length == 1) {
+                // User hasn't supplied any module names, so ask
+                def moduleString = getUserString('Enter Module Name(s - separate multiple with spaces, CapiTaliZation MatterS): ')
+                // Split it on whitespace
+                moduleList = moduleString.split("\\s+")
+            } else {
+                // User has supplied one or more module names, so pass them forward (skipping the "get" arg)
+                moduleList = args.drop(1)
+            }
+            println "List of modules to update: $moduleList"
+            for (String module: moduleList) {
+                updateModule(module)
+            }
+            break
+        case "update-all":
+            println "We're updating all modules"
+            println "List of modules:"
+            new File("modules").eachDir() { dir ->
+                String moduleName = dir.getPath().substring(8)
+                if (!excludedDependencies.contains(moduleName)) {
+                    updateModule(moduleName)
+                }
+            }
             break
         default:
             println "UNRECOGNIZED COMMAND - please try again or use 'groovyw module usage' for help"
